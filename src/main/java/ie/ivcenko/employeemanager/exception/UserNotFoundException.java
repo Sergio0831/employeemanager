5c39176e7b5a9b1e0c151579988fde52aa7b7dfb
Sergio0831 <sergejs.ivcenko@gmail.com>
2022-08-30 19:23:53 +0100

    Added HTTP methods

---
+++ src/main/java/ie/ivcenko/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package ie.ivcenko.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException  {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ie/ivcenko/employeemanager/service/EmployeeService.java
@@ -0,0 +1,42 @@
+package ie.ivcenko.employeemanager.service;
+
+import ie.ivcenko.employeemanager.exception.UserNotFoundException;
+import ie.ivcenko.employeemanager.model.Employee;
+import ie.ivcenko.employeemanager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("User not found!"));
+    }
+
+    public void deleteEmployee(Long id) {
+         employeeRepo.deleteEmployeeById(id);
+    }
+}
